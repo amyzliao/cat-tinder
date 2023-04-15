712b06e7aee971f8158a07b4a50c144c121183b5
joycexzhao <joycexzhao@gmail.com>
2023-04-15 13:41:36 -0500

    updating App.js idk

---
+++ cat-zillow/src/Components/signin.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../Styling/signin.css';
+
+const Signin = () => {
+    return (
+        <div class="signin_page">
+            <h1>Log in with Gmail!</h1>
+        </div>
+    )
+}
+
+export default Signin; 
