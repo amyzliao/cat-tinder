7635cc41ca5be69ca01c9d54a1855dfa76ff94ba
Minseo Kim <76415166+minsharky@users.noreply.github.com>
2023-04-15 21:39:04 -0500

    repush add cat

--- cat-zillow/src/Components/addcat.jsx
+++ cat-zillow/src/Components/addcat.jsx
@@ -2,13 +2,17 @@ import {React, useState} from "react";
 import { useData, setData } from '../database/firebase.js';
 import '../Styling/addcat.css';
 
-const add_new_cat = async (cats, name, desc, photo, owner) => {
+import Upload from './image_upload.jsx';
+import onFileUpload from './image_upload.jsx';
+
+const add_new_cat = (cats, name, desc, photo, owner) => {
 	const new_cat_id = cats.length;
 	const newName = name;
 	const newDesc = desc;
 	const newPhoto = photo;
 	
 	if(newName != "" & newDesc != ""){
+		alert("Cat Submitted")
 		setData(`/cats/${new_cat_id}/cat_id`, new_cat_id);
 		setData(`/cats/${new_cat_id}/name`, newName);
 		setData(`/cats/${new_cat_id}/description`, newDesc);
@@ -16,7 +20,6 @@ const add_new_cat = async (cats, name, desc, photo, owner) => {
 
 		//owner_id
 		// setData(`/cats/${new_cat_id}/owner`, owner);
-		alert("Cat Submitted")
 	}else{
 		alert("Fill in All Cat Info!")
 	}	
@@ -41,6 +44,7 @@ export const AddCat = ({cats, owner}) => {
     setUpdated(name);
 	setUpdated1(desc);
 	add_new_cat(cats, name, desc, owner)
+	onFileUpload();
   };
 
   return (
@@ -55,6 +59,7 @@ export const AddCat = ({cats, owner}) => {
 			<input type="text" onChange={handleChange1} value={desc}/> <br /><br />
 
 			{/*Label for image*/}
+			<Upload/>
 		</form>
 			<button onClick={handleClick}>Submit</button>
     </div>
