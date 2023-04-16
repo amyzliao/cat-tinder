f3041ebcd37cf4cb960f97668e2281b6f2a91bbc
AkashDeoNU <103459323+AkashDeoNU@users.noreply.github.com>
2023-04-15 21:07:24 -0500

    Update adoptcat.jsx

--- cat-zillow/src/Components/adoptcat.jsx
+++ cat-zillow/src/Components/adoptcat.jsx
@@ -1,14 +1,31 @@
 import React from "react";
-import { useData } from '../database/firebase.js';
+import { useData, setData } from '../database/firebase.js';
 import '../Styling/adoptcat.css'
 
-const AdoptCat = () => {
+const add_to_liked_list = async ( cat, user ) => {
+    const new_id = cat.cat_id;
+    const len = user.liked_list.length; 
+    try {
+        setData(user.liked_list.len, new_id);
+    } catch(error) {
+        alert(error);
+    }
+}
+
+const AdoptCat = ({data}) => {
+    const user = data.users.users[0];
+    const cat = data.users.cats[0];
+    
+    const liked_list = (event) => {
+        add_to_liked_list(cat, user);
+    }
+
     return (
     	<div class="adopt_cat">
             <img class="adopt_cat_img" src={useData('/cats/0/photo')} alt="adopt a cat!" />
             <hr />
             {/* Check mark */}
-            <button className="button">
+            <button className="button" onClick="liked_list">
                 <img className="button_img" src="https://upload.wikimedia.org/wikipedia/commons/6/6f/Eo_circle_light-green_checkmark.svg" alt="Check mark" />
             </button>
             {/* Question mark */}
